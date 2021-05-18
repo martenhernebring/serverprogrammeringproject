4e61344771333086bc4c5fbdcbe3e34a0ffb51ca
suzyrgo <suztra0803@skola.goteborg.se>
2021-05-18 16:28:09 +0200

    adde data derbt and connected to Wildfly

--- src/se/sme/dao/IntervalDaoProductVersion.java
+++ src/se/sme/dao/IntervalDaoProductVersion.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import se.sme.domain.Interval;
 
@@ -12,6 +15,8 @@ import se.sme.domain.Interval;
 @Default
 @Stateless
 public class IntervalDaoProductVersion implements IntervalDao {
+	@PersistenceContext
+	private EntityManager em;
 
     @Override
     public void insert(Interval newInterval) {
@@ -20,12 +25,16 @@ public class IntervalDaoProductVersion implements IntervalDao {
 
     @Override
     public List<Interval> findAll() {
-        List<Interval> list = new ArrayList<>();
-        var i1 = new Interval(0, 1);
-        var i2 = new Interval(20, 25);
-        list.add(i1);
-        list.add(i2);
-        return list;
+    	TypedQuery<Interval> q = em.createQuery("select employee from Employee employee", Interval.class);
+    	List<Interval> intervals = q.getResultList();
+    	return intervals;
+
+//        List<Interval> list = new ArrayList<>();
+//        var i1 = new Interval(0, 1);
+//        var i2 = new Interval(20, 25);
+//        list.add(i1);
+//        list.add(i2);
+//        return list;
 
     }
 
--- src/se/sme/domain/Interval.java
+++ src/se/sme/domain/Interval.java
@@ -1,12 +1,25 @@
 package se.sme.domain;
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @SuppressWarnings("serial")
 public class Interval implements Serializable {
-//0-39 Hours from now
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+	
+	//0-39 Hours from now
 	private int startHour;
 	private int endHour;
 
+	public Interval() {
+		//requered by JPA
+	}
 	public Interval(int startHour, int endHour){
 		this.startHour=startHour;
 		this.endHour=endHour;
