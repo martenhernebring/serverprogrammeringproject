ae0672473a8ef2facffb1f3e4db05bb26e1207b2
Erik Blidner <erik.blidner@gmail.com>
2021-05-18 10:47:42 +0200

    added arraylist

--- src/se/sme/service/IntervalManagementImplementation.java
+++ src/se/sme/service/IntervalManagementImplementation.java
@@ -1,5 +1,27 @@
 package se.sme.service;
 
-public class IntervalManagementImplementation {
+import java.util.ArrayList;
+import java.util.List;
+
+import se.sme.domain.Interval;
+
+public class IntervalManagementImplementation implements TimeManagementService {
+
+	@Override
+	public void registerInterval(Interval interval) {
+
+	}
+
+	@Override
+	public List<Interval> getAllIntervals() {
+		List<Interval> list=new ArrayList<>();
+        var i = new Interval(0, 1);
+        list.add(i);
+		return list;
+	}
+	@Override
+	public List<Interval> searchByStart(int start) {
+		return null;
+	}
 
 }
