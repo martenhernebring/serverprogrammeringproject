39b09d26b287312056655899b64bc31f32c9311d
Marten Hernebring <marten.hernebring@gmail.com>
2021-05-24 19:13:15 +0200

    Added put one to id 1 for test rest client

--- restclient/TestClient.java
+++ restclient/TestClient.java
@@ -13,36 +13,39 @@ public class TestClient {
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
         /*
-        //IntervalInstance i1 = new IntervalInstance(0, 1);
-        IntervalInstance i2 = new IntervalInstance(20, 25);
-        
-        Entity<IntervalInstance> entity = Entity.entity(i2, "application/JSON");
-        Response response = client.target("http://localhost:8080/IntervalManagment/webservice/intervals")
-                .request()
-                .buildPost(entity)
-                .invoke();
-        System.out.println(response.readEntity(IntervalInstance.class).getEndHour());
-        response.close();*/
-        
-        
-        WebTarget target = client.target("http://localhost:8080/IntervalManagement/webservice/intervals/0");
-        Invocation invocation = target.request().buildGet();
-        Response response = invocation.invoke(); 
-         
-        List<IntervalInstance> intervals = response.readEntity(new GenericType<List<IntervalInstance>>() {});
+         * //IntervalInstance i1 = new IntervalInstance(0, 1); IntervalInstance i2 = new
+         * IntervalInstance(20, 25);
+         * 
+         * Entity<IntervalInstance> entity = Entity.entity(i2, "application/JSON");
+         * Response response =
+         * client.target("http://localhost:8080/IntervalManagment/webservice/intervals")
+         * .request() .buildPost(entity) .invoke();
+         * System.out.println(response.readEntity(IntervalInstance.class).getEndHour());
+         * response.close();
+         */
+
+        /*
+         * WebTarget target = client.target(
+         * "http://localhost:8080/IntervalManagement/webservice/intervals/0");
+         * Invocation invocation = target.request().buildGet(); Response response =
+         * invocation.invoke();
+         * 
+         * List<IntervalInstance> intervals = response.readEntity(new
+         * GenericType<List<IntervalInstance>>() {}); for (IntervalInstance ii :
+         * intervals) { System.out.println(ii); } response.close();
+         */
+
+        Response response = client.target("http://localhost:8080/IntervalManagement/webservice/intervals?id=1")
+                .request().buildPut(null).invoke();
+        response.close();
+        response = client.target("http://localhost:8080/IntervalManagement/webservice/intervals").request()
+                .buildGet().invoke();
+        //System.out.println(response.readEntity(String.class));
+        List<IntervalInstance> intervals = response.readEntity(new GenericType<List<IntervalInstance>>() {
+        });
         for (IntervalInstance ii : intervals) {
             System.out.println(ii);
         }
         response.close();
-        /*Response response = client.target("http://localhost:8080/IntervalManagement/webservice/intervals")
-                         .request()
-                         .buildGet()
-                         .invoke();
-        //System.out.println(response.readEntity(String.class));
-        List<IntervalInstance> intervals = response.readEntity(new GenericType<List<IntervalInstance>>() {});
-        for (IntervalInstance ii : intervals) {
-            System.out.println(ii);
-        }
-        response.close();*/
     }
 }
