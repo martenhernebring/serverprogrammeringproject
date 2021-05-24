b3cd88b43e20c01eae81bffd1dfed5c26fceb3d9
Marten Hernebring <marten.hernebring@gmail.com>
2021-05-24 11:28:04 +0200

    Added service class

--- src/se/sme/rest/IntervalResource.java
+++ src/se/sme/rest/IntervalResource.java
@@ -13,31 +13,30 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.QueryParam;
-
 
 import se.sme.domain.IntervalInstance;
 import se.sme.service.TimeManagementService;
+
 @Stateless
 @Path("/intervals")
 public class IntervalResource {
-	@Inject
-	private TimeManagementService service;
-	
-	@GET
-	@Produces("application/JSON")
-	public List<IntervalInstance> getAllInstances() {
-		return service.getAllIntervals();
-	}
-	
-	@GET
+    @Inject
+    private TimeManagementService service;
+
+    @GET
+    @Produces("application/JSON")
+    public List<IntervalInstance> getAllInstances() {
+        return service.getAllIntervals();
+    }
+
+    @GET
     @Produces("application/JSON")
     @Path("{startNo}")
     public List<IntervalInstance> getIntervalByStart(@PathParam("startNo") int hour) {
         return service.searchByStart(hour);
     }
-    
+
     @POST
     @Produces("application/JSON")
     @Consumes("application/JSON")
@@ -45,38 +44,24 @@ public class IntervalResource {
         service.registerInterval(interval);
         return interval;
     }
-    
+
     @DELETE
     @Path("{hour}")
-    public Response delete(@PathParam("hour") int hour) {
+    public void delete(@PathParam("hour") int hour) {
         List<IntervalInstance> li = service.searchByStart(hour);
-        for (IntervalInstance i:li){
-        	service.delete(i);
+        for (IntervalInstance i : li) {
+            service.delete(i);
         }
-        
-
-        return Response.ok().build();
-        
-        
+        // return Response.ok().build();
     }
+
     @PUT
-    public Response update(@DefaultValue("1") @QueryParam("hour") Integer hour, @QueryParam("id") Integer id) {
-    	if (hour >= 0 && id != null) {
-    		//return Response.ok().build();
-    		return Response.ok ().build();
-    		}
-		return null;
-  
-    		//return Response.status(400).build();
-    		//}
-    	
-       // Todo updateTodo = todoDAO.findById(hour);
-
-        //updateTodo.setTask(todo.getTask());
-       // updateTodo.setDescription(todo.getDescription());
-        //todoDAO.update(updateTodo);
-
-       // return Response.ok().build();
+    public void update(@DefaultValue("1") @QueryParam("hour") Integer hour, @QueryParam("id") Integer id) {
+        if (hour >= 0 && id != null) {
+            // return Response.ok().build();
+            service.addHours(hour, id);
+        }
+        // return Response.status(400).build();
     }
 
 }
